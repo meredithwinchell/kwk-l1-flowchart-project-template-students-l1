9d1c2b9d2bbe2a87e7fe3c5ab2b5f0ee5230c997
meredith winchell <meredithwinchell@gmail.com>
2018-06-14 20:49:36 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,133 @@
+Movies = {
+
+    genre: {
+
+      romance: {
+        romcom: ['Clueless', 'Theres Something About Mary', '17 Again', 'Juno',
+          'Lonesome Jim', 'Sideways', 'Breakfast at Tiffanys',
+          'Eternal Sunshine of the Spotless Mind'],
+        romdrama: ['Rebel Without a Cause', 'Juno', 'Titanic', 'Forrest Gump',
+          'Walking The Dead', 'On Golden Pond', 'Casablanca', 'Gone with the Wind',
+          'Slumdog Millionaire', 'Eternal Sunshine of the Spotless Mind'],
+        tearjerker: ['Titanic'],
+        allromance: ['Clueless', 'Theres Something About Mary', '17 Again', 'Juno',
+           'Rebel Without a Cause', 'Juno', 'Titanic', 'Forrest Gump', 'Titanic',
+           'Fast Times at Ridgemont High', 'Lonesome Jim', 'Walking The Dead',
+           'On Golden Pond', 'Sideways', 'Casablanca', 'Gone with the Wind',
+           'Breakfast at Tiffanys', 'Slumdog Millionaire',
+           'Eternal Sunshine of the Spotless Mind']
+      },
+
+
+      drama: {
+        true: ['The Blind Side', 'Into the Wild', 'Schlinders List', 'Raging Bull',
+          'Straight Outta Compton', 'Lion'],
+        crime: ['Oceans series', 'Heathers', 'Antibodies', 'Murder in the First',
+          'Knockin on Heavens Door', 'The Godfather', 'Shawkshank Redemption',
+          'Straight Outta Compton', 'Boyz n the Hood', 'Pulp Fiction',
+          'Reservoir Dogs', 'Slumdog Millionaire'],
+        military: ['Top Gun', 'City of Life and Death', 'Dunkirk', 'Casablanca',
+          'Gone with the Wind', 'Gladiator', 'Braveheart', '300'],
+        historical: ['Titanic', 'City of Life and Death', 'The Color Purple',
+          'Dunkirk', 'Schlinders List', 'Gone with the Wind', 'Gladiator',
+          'Braveheart', '300'],
+        mystery: ['IT', 'The Shining', 'Walking The Dead', 'Murder in the First',
+          'Citizen Kane', 'Scream', 'Reservoir Dogs'],
+        romdrama: ['Rebel Without a Cause', 'Juno', 'Titanic', 'Forrest Gump',
+          'Walking The Dead', 'On Golden Pond', 'Casablanca', 'Gone with the Wind',
+          'Slumdog Millionaire', 'Eternal Sunshine of the Spotless Mind'],
+        alldrama: ['Lady Bird', 'The Blind Side', 'Oceans series', 'Heathers',
+           'Top Gun', 'Titanic', 'IT', 'The Shining', 'Rebel Without a Cause',
+           'Juno', 'Titanic', 'Forrest Gump', 'City of Life and Death',
+           'Antibodies', 'Manic', 'The Beautiful Country', 'Walking The Dead',
+           'Murder in the First', 'Driving Miss Daisy', 'Pleasantville',
+           'On Golden Pond', 'The Color Purple', 'Being There',
+           'Knockin on Heavens Door', 'Sideways', 'Into the Wild', 'Dunkirk',
+           'Harry Potter series', 'Hobbit series', 'The Godfather',
+           'Shawkshank Redemption', 'Schlinders List', 'Raging Bull', 'Casablanca',
+           'Citizen Kane', 'Straight Outta Compton', 'Boyz n the Hood',
+           'Divergent series', 'Gone with the Wind', 'Breakfast at Tiffanys',
+           'Pulp Fiction', 'Scream', 'The Breakfast Club', 'Lion',
+           'Its a Wonderful Life', 'Jaws', 'Reservoir Dogs', 'Rocky series',
+           'Slumdog Millionaire', 'Amadeous', 'Gladiator', 'Braveheart', '300',
+           'Eternal Sunshine of the Spotless Mind']
+      },
+
+      horror: {
+        comedyhor: [],
+        psychhor: ['The Shining', 'Split'],
+        supernaturalhorror: ['The Conjuring series', 'Annabelle series'],
+        realhorror: ['Get Out', 'Scream'],
+        cyberhorror: [],
+        actionhorror: ['IT'],
+        slasher: ['Halloween', 'Scream'],
+        allhorror: ['The Shining', 'Split', 'Get Out', 'IT', 'Halloween',
+          'Horror Express', 'Scream', 'The Conjuring series',
+          'Annabelle series']
+      },
+
+      fantasy: ['Spaceballs', 'Pleasantville', 'Harry Potter series',
+        'Hobbit series', 'Space Jam', 'Divergent series', 'Its a Wonderful Life'],
+
+
+
+      documentary: ['Blackfish', 'Man on Wire', '13th'],
+
+      comedy: {
+        romcom: ['Clueless', 'Theres Something About Mary', '17 Again', 'Juno',
+          'Lonesome Jim', 'Sideways', 'Breakfast at Tiffanys',
+          'Eternal Sunshine of the Spotless Mind'],
+        allcomedy: ['Ferris Buellers Day Off', 'Clueless', 'Oceans series',
+          'Deadpool series', 'Teenage Mutant Ninja Turtles series',
+           'Theres Something About Mary', 'Heathers', 'Clueless', '17 Again',
+           'National Lampoons series', 'Monty Python series',
+           'Caddyshack', 'Spaceballs', 'Dumb and Dumber series', 'Happy Gilmore',
+           'Tommy Boy', 'Ace Ventura: Pet Detective', 'Hot Shots!', 'The Waterboy',
+           'Lady Bird', 'Jawbreaker', 'Fast Times at Ridgemont High',
+           'The Winning Season', 'Juno', 'Lonesome Jim', 'Pleasantville',
+           'My Cousin Vinny', 'Being There', 'Knockin on Heavens Door', 'Sideways',
+           'Dazed and Confused', 'Space Jam', 'Breakfast at Tiffanys',
+           'Pulp Fiction', 'The Breakfast Club', 'Its a Wonderful Life', 'Amadeous',
+           'Eternal Sunshine of the Spotless Mind']
+      },
+
+      action: {
+        spy: [],
+        govwar: ['Hot Shots!', 'Top Gun', 'Dunkirk', 'Divergent series', 'Gladiator',
+          'Braveheart', '300'],
+        crime: ['Oceans series', 'Ace Ventura: Pet Detective', 'Red Sun',
+          'Knockin on Heavens Door'],
+        superhero: ['Spider-Man series', 'Wonder Woman', 'Deadpool series',
+           'Teenage Mutant Ninja Turtles series'],
+        allaction: ['IT', 'Ace Ventura: Pet Detective', 'Hot Shots!',
+          'Top Gun', 'Oceans series', 'Ace Ventura: Pet Detective',
+          'Spider-Man series', 'Wonder Woman', 'Deadpool series',
+          'Teenage Mutant Ninja Turtles series', 'Red Sun',
+          'Knockin on Heavens Door', 'Into the Wild', 'Dunkirk',
+          'Harry Potter series', 'Hobbit series', 'Divergent series', 'Jaws',
+          'Rocky series', 'Gladiator', 'Braveheart', '300']
+      },
+
+      throwback: ['Ferris Buellers Day Off', 'Clueless', 'The Shining',
+         'Halloween', 'National Lampoons series', 'Monty Python series',
+         'Caddyshack', 'Spaceballs', 'Dumb and Dumber series', 'Tommy Boy',
+         'Happy Gilmore', 'Hot Shots!', 'The Waterboy', 'Rebel Without a Cause',
+         '17 Again', 'Titanic', 'Forrest Gump', 'Jawbreaker', 'Driving Miss Daisy',
+         'On Golden Pond', 'The Color Purple', 'Being There', 'Shawkshank Redemption',
+         'The Godfather', 'Schlinders List', 'Casablanca', 'Dazed and Confused',
+         'Boyz n the Hood', 'Space Jam', 'Breakfast at Tiffanys', 'Pulp Fiction',
+         'Scream', 'The Breakfast Club', 'Its a Wonderful Life', 'Jaws',
+         'Rocky series'],
+
+      sport: ['The Blind Side', 'Caddyshack', 'Happy Gilmore', 'The Waterboy',
+        'The Winning Season', 'Raging Bull', 'Space Jam', 'Rocky series'],
+
+      sad: ['The Blind Side', 'Schlinders List']
+
+    },
+
+}
+
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -12,18 +142,19 @@ class ApplicationController < Sinatra::Base
   get '/quiz/1' do
     erb :adult1
   end
-
-
-  post '/adult/1' do
+=begin
+  get '/adult/1' do
     erb :adultromance
   end
+=end
+  post '/adult/1' do
+    "My selection was #{Movies[:genre][:romance][:romcom]}."
+    #erb :adultromance
+  end
+
 
   post '/adult/romance' do
     erb :adultdrama
   end
-
-
-
-
 
 end
