d84585d968b720983e413603b80ef26e50cf9163
meredith winchell <meredithwinchell@gmail.com>
2018-06-14 19:17:44 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,24 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :welcome
+  end
+
+  get '/quiz/1' do
+    erb :quiz1
+  end
+
+  post '/quiz/1' do
+    erb :adult1
+  end
+
+  post '/adult/1' do
+    erb :adultromance
+  end
+
+
+  
+
+
 end
